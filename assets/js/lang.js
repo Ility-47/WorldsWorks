9271427dd802e39b736cb36fb36090fdb828042b
Илья <64031009+Ility-47@users.noreply.github.com>
2023-03-01 19:40:59 +0300

    Add files via upload

---
+++ assets/js/lang.js
@@ -0,0 +1,14 @@
+const langArr = {
+    "inTitle":{
+        "ru":"Удаленная работа <br/> с проектами со всего мира",
+        "en":"Remote project work <br/> from all over the world",    
+    },
+    "inSub":{
+        "ru":"Возможность просмотра сотен текущих удаленных проектов <br/> из десятков стран в одном месте - одновременно.",
+        "en":"The ability to view hundreds of current remote projects from a <br/> dozen countries in one place - simultaneously."
+    },
+    "iBtn":{
+        "ru":"Посмотреть",
+        "en":"Сhoose a tariff",
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ let logo = document.querySelector('.logo__img');
 let title = document.querySelector('.info__title');
 let iBtn = document.querySelector('.info__btn');
 let themePos = document.querySelector('.buttons__theme h5');
+const subTitle = document.querySelector('.info__subtitle h4');
+const selectLng = document.querySelector('.button__lng');
 /*
 document.ready(function()
 {
@@ -19,14 +21,43 @@ let start = () =>{
         btn.classList.toggle('switchOn');
         title.classList.toggle('info__title_wh');
         iBtn.classList.toggle('info__btn_wh');
+        selectLng.classList.toggle('button__lng_wh')
         if(btn.classList.value == 'switch'){
             themePos.textContent = 'Theme: dark';
   
         }else{
             themePos.textContent = 'Theme: light';   
-        }
+        }   
         
+    })
+    selectLng.addEventListener("click", () =>{
+        console.log(selectLng.value);
+        if(selectLng.value == 'en'){
+            selectLng.value = 'ru';
+            selectLng.textContent = 'Рус'
+
+            title.innerHTML = langArr['inTitle']["ru"];
+            subTitle.innerHTML = langArr['inSub']["ru"];
+            iBtn.innerHTML = langArr["iBtn"]["ru"];
+        }else if(selectLng.value == 'ru'){
+            selectLng.value = 'en';
+            selectLng.textContent = 'En'   
+
+            title.innerHTML = langArr['inTitle']["en"];
+            subTitle.innerHTML = langArr['inSub']["en"];
+            iBtn.innerHTML = langArr["iBtn"]["en"]; 
+        }
+
     })
 }
 
+/* Изменение языка */
+
+const allLeng = ['ru', 'en'];
+
+function changeLang(){
+    let hash = window.location.hash;
+    console.log(hash)
+}
+changeLang();
 start()
